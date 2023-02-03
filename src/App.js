26d7ff28b48024f12c637648d98f02aa99dd3d5d
kunalchhabra37 <kunalchharba37@gmail.com>
2023-02-03 09:41:51 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { CopyBlock, dracula } from "react-code-blocks";
+
+const API_KEY = '' // get your api key from app.conciselabs.io
 
 function App() {
+  const [wallet, setWallet] = useState("")
+  const [result, setResult] = useState("")
+
+  const fetchNFTs = async () => {
+    let res = await fetch(
+      `https://app.conciselabs.io/api/v0/nfts/solana-mainnet/${wallet}/get-holder-nfts/${API_KEY}`
+    )
+      .then((res) => res.json())
+      .catch((err) => {
+        console.log(err);
+        setResult(err);
+      });
+    setResult(res);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-3xl">Fetch NFTs holded by a Wallet</h1>
+        <div className="pl-7 ">
+          <div className="pt-[10px] flex pr-[34px] flex-col">
+            <input
+              type="text"
+              placeholder="Enter Solana Wallet Address"
+              onChange={(e) => {
+                setWallet(e.target.value);
+              }}
+              value={wallet}
+              className="bg-bg-[#323131] border border-[#B0B0B0] w-[505px] text-sm rounded-sm p-2.5 dark:bg-[#323131] dark"
+            />
+<br />
+            <button
+              onClick={fetchNFTs}
+              className="bg-gradient-to-r from-[#F55151] to-[#FFB800] text-base w-[161px] h-[46px] py-2 px-4 rounded mt-[20px] mb-[20px]"
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+        {result && (
+      <div className="flex h-full w-[750px] ml-[15px] mb-[10px] text-left">
+          <CopyBlock
+            text={
+               JSON.stringify(result, null, 4)
+            }
+            language={"json"}
+            showLineNumbers={true}
+            theme={dracula}
+            codeBlock
+          />
+      </div>
+        )}
     </div>
   );
 }
